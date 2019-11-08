60c062205d8e2eaae6551d3fa17860f825f7f55f
Aayush Shah <shahaayush1999@github.com>
2019-11-08 14:08:30 +0530

    Added debug panel for Edit profile page

--- edit.php
+++ edit.php
@@ -1,34 +1,4 @@
-<?php
-	include('session.php');
-	include('config.php');
-
-	$fname = $mname = $lname = $pnnumber = $email = $live_back = $dead_back = $prev_int = $branch_id = $cgpa = $batch = $year_id = "";
-
-	if(isset($_POST['fname']))	$fname = $_POST["fname"];
-	if(isset($_POST['mname']))	$mname = $_POST["mname"];
-	if(isset($_POST['lname']))	$lname = $_POST["lname"];
-	if(isset($_POST['pnumber']))	$pnumber = $_POST["pnumber"];
-	if(isset($_POST['email']))	$email = $_POST["email"];
-	if(isset($_POST['live_back']))	$live_back = $_POST["live_back"];
-	if(isset($_POST['dead_back']))	$dead_back = $_POST["dead_back"];
-	if(isset($_POST['previous_internships']))	$fname = $_POST["previous_internships"];
-	if(isset($_POST['branch_id']))	$branch_id = $_POST["branch_id"];
-	if(isset($_POST['cgpa']))	$cgpa = $_POST["cgpa"];
-	if(isset($_POST['batch']))	$batch = $_POST["batch"];
-	if(isset($_POST['year_id']))	$year_id = $_POST["year_id"];
-
-	$query = "INSERT INTO `student`(`mis`, `first_name`, `middle_name`, `last_name`, `number`, `email`, `live_back`, `dead_back`, `previous_internships`, `branch_id`, `cgpa`, `batch`, `year_id`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-	if($stmt = $con->prepare($query)){
-		$stmt->bind_param("sssssssssssss", $login_mis, $fname, $mname, $lname, $pnumber, $email, $live_back, $dead_back, $prev_int, $branch_id, $cgpa, $batch, $year_id);
-		$stmt->execute();
-		echo "$stmt->affected_rows row inserted";
-	} else {
-			$error = "Not connected";
-			echo "$error";
-	}
-?>
-
- <!doctype html>
+<!doctype html>
  <html lang="en">
 	<head>
 		<meta charset="utf-8">
@@ -39,24 +9,44 @@
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"> </script>
 	</head>
 	<body>
-		<div class="row d-flex"> <!-- justify-content-center -->
+		<div class="row d-flex"> <!-- Add this class to recenter edit div after removing debug panel "justify-content-center" -->
 
-			<div class="col-md-4 border">
-				<h2 class="text-center">Debug</h2>
+			<div class="col-md-4">
+				<h2 class="text-center">Processing Panel</h2>
 				<table>
-					<?php
-					    foreach ($_POST as $key => $value) {
-					        echo "<tr>";
-					        echo "<td>";
-					        echo $key;
-					        echo "</td>";
-					        echo "<td>";
-					        echo $value;
-					        echo "</td>";
-					        echo "</tr>";
-					    }
-					?>
+					<!-- Post Var dump -->
+					<?php foreach ($_POST as $key => $value) echo "<tr>\n<td>".$key."</td>\n<td>".$value."</td>\n</tr>"; ?>
 				</table>
+				<?php
+					include('session.php');
+					include('config.php');
+
+					$fname = $mname = $lname = $pnnumber = $email = $live_back = $dead_back = $prev_int = $branch_id = $cgpa = $batch = $year_id = "";
+
+					if(isset($_POST['fname']))	$fname = $_POST["fname"];
+					if(isset($_POST['mname']))	$mname = $_POST["mname"];
+					if(isset($_POST['lname']))	$lname = $_POST["lname"];
+					if(isset($_POST['pnumber']))	$pnumber = $_POST["pnumber"];
+					if(isset($_POST['email']))	$email = $_POST["email"];
+					if(isset($_POST['live_back']))	$live_back = $_POST["live_back"];
+					if(isset($_POST['dead_back']))	$dead_back = $_POST["dead_back"];
+					if(isset($_POST['previous_internships']))	$prev_int = $_POST["previous_internships"];
+					if(isset($_POST['branch_id']))	$branch_id = $_POST["branch_id"];
+					if(isset($_POST['cgpa']))	$cgpa = $_POST["cgpa"];
+					if(isset($_POST['batch']))	$batch = $_POST["batch"];
+					if(isset($_POST['year_id']))	$year_id = $_POST["year_id"];
+
+					$query = "INSERT INTO `student`(`mis`, `first_name`, `middle_name`, `last_name`, `number`, `email`, `live_back`, `dead_back`, `previous_internships`, `branch_id`, `cgpa`, `batch`, `year_id`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+					if($stmt = $con->prepare($query)){
+						$stmt->bind_param("sssssssssssss", $login_mis, $fname, $mname, $lname, $pnumber, $email, $live_back, $dead_back, $prev_int, $branch_id, $cgpa, $batch, $year_id);
+						$stmt->execute();
+						// Printing Last response from server
+						echo "<br><br>Server Response: ".$con->error;
+					} else {
+							$error = "Not connected";
+							echo "$error";
+					}
+				?>
 			</div>
 			<div class="col-md-4 border rounded">
 				<h2 class="text-center">Edit Profile</h2>
