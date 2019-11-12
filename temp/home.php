46e7728d5dc485a57ac3845cb24be57d713812dd
Aayush Shah <shahaayush1999@github.com>
2019-11-12 19:10:32 +0530

    Fixed temp/home.php and welcome.php for working delete button

--- temp/home.php
+++ temp/home.php
@@ -1,23 +1,17 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "internship_portal";
+	include('../session.php');
+	include('../config.php');
+	include('../functions.php');
 
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-	die("Connection failed: " . $conn->connect_error);
-}
-if (isset($_POST)) {
-	foreach($_POST as $key => $value) {
-	    if (strstr($key, 'delete')){
-	    	$id = substr($key, 7);
-	    	$query = "delete from branch where branch_id = ".$id;
-	    	$result = $conn->query($query);
-	    }
+	if (isset($_POST)) {
+		foreach($_POST as $key => $value) {
+		    if (strstr($key, 'delete')){
+		    	$id = substr($key, 7);
+				$query_error = sql_delete_query('branch_id', $id, 'branch');
+				echo $query_error;
+		    }
+		}
 	}
-}
 ?>
 
 <html>
@@ -26,32 +20,34 @@ if (isset($_POST)) {
 
 </head>
 <body>
-    <table class="table table-striped">
-    	<thead class="thead-dark">
-	    	<tr>
-	    		<th>ID</th>
-	    		<th>Name</th>
-	    		<th>Delete</th>
-	    	</tr>
-    	</thead>
-    	<tbody>
-    		<?php
-	    		$sql = "SELECT branch_id, branch_name FROM branch ORDER BY branch_id";
-				$result = $conn->query($sql);
-				if ($result->num_rows > 0) {
-				    // output data of each row
-				    while($row = $result->fetch_assoc()) {
-				        echo "<tr><td>".$row["branch_id"]."</td><td>".$row["branch_name"]."</td>";
-				        echo "<td><button name=\"delete_".$row['branch_id']."\">Delete</td>";
-				        echo '</tr>';
-				    }
-				    echo "</table>";
-				} else {
-					echo "0 results";
-				}
-    		?>
-    	</tbody>
-	</table>
-</body>
+	<form action="home.php" method="POST">
+	    <table class="table table-striped">
+	    	<thead class="thead-dark">
+		    	<tr>
+		    		<th>ID</th>
+		    		<th>Name</th>
+		    		<th>Delete</th>
+		    	</tr>
+	    	</thead>
+	    	<tbody>
+	    		<?php
+		    		$sql = "SELECT branch_id, branch_name FROM branch ORDER BY branch_id";
+					$result = $con->query($sql);
+					if ($result->num_rows > 0) {
+					    // output data of each row
+					    while($row = $result->fetch_assoc()) {
+					        echo "<tr><td>".$row["branch_id"]."</td><td>".$row["branch_name"]."</td>";
+					        echo "<td><button name=\"delete_".$row['branch_id']."\">Delete</td>";
+					        echo '</tr>';
+					    }
+					    echo "</table>";
+					} else {
+						echo "0 results";
+					}
+	    		?>
+	    	</tbody>
+		</table>
+	</form>
+
 </html>
-<?php $conn->close(); ?>
+<?php $con->close(); ?>
--- welcome.php
+++ welcome.php
@@ -1,7 +1,21 @@
 <?php
+	// include('session.php');
+	// include('config.php');
+	// include('functions.php');
+	//
+	// if (isset($_POST)) {
+	// 	foreach($_POST as $key => $value) {
+	// 	    if (strstr($key, 'delete')){
+	// 	    	$id = substr($key, 7);
+	// 			$query_error = sql_delete_query('branch_id', $id, 'branch');
+	// 			echo $query_error;
+	// 	    }
+	// 	}
+	// }
 	include('session.php');
 	include('config.php');
 	include('functions.php');
+
 	if (isset($_POST)) {
 		foreach($_POST as $key => $value) {
 		    if (strstr($key, 'delete')){
@@ -54,37 +68,39 @@
 				<a href = "logout.php" class="btn btn-danger my-2 my-sm-0" type="submit">Sign Out</a>
 			</div>
 		</nav>
-		<table class="table table-striped">
-			<thead class="thead-dark">
-				<tr>
+		<form action="welcome.php" method="POST">
+			<table class="table table-striped">
+				<thead class="thead-dark">
+					<tr>
+						<?php
+							$headers = array('ID', 'Name', 'Delete');
+							place_cells('th', $headers);
+						?>
+					</tr>
+				</thead>
+				<tbody>
 					<?php
-						$headers = array('ID', 'Name', 'Delete');
-						place_cells('th', $headers);
-					 ?>
-
-				</tr>
-			</thead>
-			<tbody>
-				<?php
-					//$variables = array('branch_id', 'branch_name');
-					//$table_name = 'branch';
-					$sql = "SELECT branch_id, branch_name FROM branch";
-					$result = $con->query($sql);
-					if ($result->num_rows > 0) {
-						// output data of each row
-						while($row = $result->fetch_assoc()) {
-							echo "<tr><td>".$row["branch_id"]."</td><td>".$row["branch_name"]."</td>";
-							echo "<td><button name=\"delete_".$row['branch_id']."\">Delete</td></tr>";
+						//$variables = array('branch_id', 'branch_name');
+						//$table_name = 'branch';
+						$sql = "SELECT branch_id, branch_name FROM branch";
+						$result = $con->query($sql);
+						if ($result->num_rows > 0) {
+							// output data of each row
+							while($row = $result->fetch_assoc()) {
+								echo "<tr><td>".$row["branch_id"]."</td><td>".$row["branch_name"]."</td>";
+								echo "<td><button name=\"delete_".$row['branch_id']."\">Delete</td></tr>";
+							}
+							echo "</table>";
+						} else {
+							echo "0 results";
 						}
-						echo "</table>";
-					} else {
-						echo "0 results";
-					}
-				?>
-			</tbody>
-		</table>
+					?>
+				</tbody>
+			</table>
+		</form>
 		<!-- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script> -->
+		<?php $con->close(); ?>
 	</body>
 </html>
